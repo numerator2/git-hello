27e08b08c1ea53ded7144c1fd8b0820149159a50
Mark Newman <mwnewman@umich.edu>
2018-01-25 05:32:36 -0500

    trying harder merge

--- hello.py
+++ hello.py
@@ -1,4 +1,4 @@
 print ('hello, my name is Mark')
-print ('my birthday is in october')
+print ('my birthday is in November')
 print ('hello, my name is also Mark')
 print ('my birthday is also in October')
