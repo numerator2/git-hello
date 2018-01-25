255ad454b1660490920623a1263ad4b634cca5e4
mwnewman <mwnewman@umich.edu>
2018-01-25 12:34:46 -0500

    added birthplace in master

--- hello.py
+++ hello.py
@@ -5,3 +5,5 @@ print ('hello, my name is also Mark')
 print ('my birthday is also in October')
 print ('My favorite food is also Massman Curry. We have so much in common!')
 print ('My favorite kind of music is post-rock instrumental')
+print ('I was born in Texas')
+## here I'm going to
